9ee247a11b804dca31df3734aaa3989e95705b1a
tuan0342 <79151156+tuan0342@users.noreply.github.com>
2023-03-29 15:14:59 +0700

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Code gồm có: template + logic
+// JSX (chính là template)
+// trình biên dịch là babel
+// src={logo}: là biến 
+
+// function App() {}      <=>      function App = () => {}
+
+// Xóa 1 vài file ko dùng: 
+// + App.test.js (dùng để test code)
+// + setupTests.js
+
+
 function App() {
   return (
     <div className="App">
