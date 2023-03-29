99fbf1b2928e7408bcf3c4c6801f6e5eab7fb0cc
tuan0342 <79151156+tuan0342@users.noreply.github.com>
2023-03-29 16:24:34 +0700

    video 6 add scss

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,10 @@
-import './Navigation.css';
+import './Navigation.scss';
 //import '../components/Navigation.css';
 
 const Nav = () => {
     return (
-        <div class="topnav">
-            <a class="active" href="#home">Home</a>
+        <div className='topnav'>
+            <a className='active' href="#home">Home</a>
             <a href="#news">News</a>
             <a href="#contact">Contact</a>
             <a href="#about">About</a>
